21d93200a87b0dffe31dcc3b21ac187448d15844
Shankar Sharma <35638442+shankar91sharma@users.noreply.github.com>
2021-07-10 13:43:40 +0530

    Update main.js
    weather api id update for test.

--- public/js/main.js
+++ public/js/main.js
@@ -19,7 +19,7 @@ const getInfo = async(event) => {
         try{
            
 
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=c4645b01e0ce2d911e1f22f335a82e3e`
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=********`
             const response = await fetch(url);
 
             const data = await response.json();
